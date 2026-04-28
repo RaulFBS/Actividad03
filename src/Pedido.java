6a5b998f00b2fe307ea73f5e7f4f84defa9c1d01
raul <rs6626808@gmail.com>
2026-04-28 18:30:53 -0500

    Clase pedido completada

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,3 +1,47 @@
 public class Pedido {
+    private Cliente cliente;
+    private double monto;
+    private boolean estado;
 
+
+    public Pedido(Cliente cliente, double monto, boolean estado) {
+        this.cliente = cliente;
+        this.monto = monto;
+        this.estado = estado;
+    }
+
+
+    public Pedido() {
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public void validarPedido() {
+        System.out.println("Validando pedido...");
+        if (estado) {
+            System.out.println("Pedido validado correctamente");
+        }
+    }
 }
