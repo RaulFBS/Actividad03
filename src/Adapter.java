072dc94aad9e323bde5f55a795e8fa096ff74e6b
raul <rs6626808@gmail.com>
2026-04-28 18:39:52 -0500

    Clase adapter completa

--- src/Adapter.java
+++ src/Adapter.java
@@ -1,2 +1,17 @@
-public class Adapter {
+public class Adapter implements FacturaService{
+    private LegacyBillingSystem legacyBillingSystem;
+    public Adapter(LegacyBillingSystem legacyBillingSystem){
+        this.legacyBillingSystem = legacyBillingSystem;
+    }
+
+    @Override
+    public void emitirFactura(Cliente cliente, double monto) {
+        String nombreCliente = cliente.getNombre();
+        String montoTexto = String.valueOf(monto);
+
+        legacyBillingSystem.crearBoletaElectronica(nombreCliente,montoTexto);
+
+        System.out.println("Factura emitidada com exito");
+
+    }
 }
