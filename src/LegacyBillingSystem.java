4c83fc278afeedc01b54cf759ec53feb9feb8a87
raul <rs6626808@gmail.com>
2026-04-28 18:35:57 -0500

    Clase legacy completa

--- src/LegacyBillingSystem.java
+++ src/LegacyBillingSystem.java
@@ -1,2 +1,6 @@
 public class LegacyBillingSystem {
+    public void crearBoletaElectronica(String nombreCliente,String totalCuenta){
+        System.out.println("Creando Boleta de Electronica "+nombreCliente);
+        System.out.println("El cuenta es: "+totalCuenta);
+    }
 }
