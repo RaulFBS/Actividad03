91d4affb90557949b7f0759930c12ffc080a8a5e
raul <rs6626808@gmail.com>
2026-04-28 18:25:01 -0500

    Creacion de clases

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Cliente {
+    private String nombre;
+    private String dni;
+
+
+    public Cliente(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public Cliente() {
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
